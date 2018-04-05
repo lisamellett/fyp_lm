94109aa69dbae9db51583fff29b21a462a113913
lisa <lisa.mellet@teamwork.com>
2018-04-05 13:52:48 +0100

    dedent

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,7 +9,7 @@ import Employees from '../components/Employees';
 import Profile from '../components/Profile';
 import Performance from '../components/Performance';
 import MyPerformance from '../components/MyPerformance';
-import Metrics from '../components/Metrics';
+import Metrics from '../components/MetricsTest';
 
 Vue.use(Router);
 
